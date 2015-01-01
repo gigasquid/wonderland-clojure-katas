8051dc0b414a163f4e7d89839e18ed2c2af2b0c8
gigasquid <cmeier@gigasquidsoftware.com>
2015-01-01 11:13:19 -0500

    make tests more general to handle multiple solutions https://github.com/gigasquid/wonderland-clojure-katas/issues/16

--- fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
+++ fox-goose-bag-of-corn/test/fox_goose_bag_of_corn/puzzle_test.clj
@@ -2,25 +2,43 @@
   (:require [clojure.test :refer :all]
             [fox-goose-bag-of-corn.puzzle :refer :all]))
 
-(def solution
-  [[[:fox :goose :corn :you] [:boat] []]
-   [[:fox :corn] [:boat :you :goose] []]
-   [[:fox :corn] [:boat] [:goose :you]]
-   [[:fox :corn] [:boat :you] [:goose]]
-   [[:fox :corn :you] [:boat] [:goose]]
-   [[:fox] [:corn :boat :you] [:goose]]
-   [[:fox] [:boat] [:goose :you :corn]]
-   [[:fox] [:boat :you :goose] [:corn]]
-   [[:fox :goose :you] [:boat] [:corn]]
-   [[:goose] [:boat :you :fox] [:corn]]
-   [[:goose] [:boat] [:corn :you :fox]]
-   [[:goose] [:boat :you] [:corn :fox]]
-   [[:goose :you] [:boat] [:corn :fox]]
-   [[] [:boat :you :goose] [:corn :fox]]
-   [[] [:boat] [:corn :fox :goose :you]]])
-
+(defn validate-move [step1 step2]
+  (testing "only you and another thing can move"
+    (let [diff1 (clojure.set/difference step1 step2)
+          diff2 (clojure.set/difference step2 step1)
+          diffs (concat diff1 diff2)
+          diff-num (count diffs)]
+      (is (> 3 diff-num))
+      (when (pos? diff-num)
+        (is (contains? (set diffs) :you)))
+      step2)))
 
 (deftest test-river-crossing-plan
-  (testing "the fox, goose, corn, and you all made it to the other side of the river"
-    (is (= (map (partial map set) solution)
-           (map (partial map set) (river-crossing-plan))))))
+  (let [crossing-plan (map (partial map set) (river-crossing-plan))]
+    (testing "you begin with the fox, goose and corn on one side of the river"
+      (is (= [#{:you :fox :goose :corn} #{:boat} #{}]
+             (first crossing-plan))))
+    (testing "you end with the fox, goose and corn on one side of the river"
+      (is (= [#{} #{:boat} #{:you :fox :goose :corn}]
+             (last crossing-plan))))
+    (testing "things are safe"
+      (let [left-bank (map first crossing-plan)
+            right-bank (map last crossing-plan)]
+        (testing "the fox and the goose should never be left alone together"
+          (is (empty?
+               (filter #(= % #{:fox :goose}) (concat left-bank right-bank)))))
+        (testing "the goose and the corn should never be left alone together"
+          (is (empty?
+               (filter #(= % #{:goose :corn}) (concat left-bank right-bank)))))))
+    (testing "The boat can carry only you plus one other"
+      (let [boat-positions (map second crossing-plan)]
+        (is (empty?
+             (filter #(> (count %) 3) boat-positions)))))
+    (testing "moves are valid"
+      (let [left-moves (map first crossing-plan)
+            middle-moves (map second crossing-plan)
+            right-moves (map last crossing-plan)]
+        (reduce validate-move left-moves)
+        (reduce validate-move middle-moves)
+        (reduce validate-move right-moves )))))
+
