820b18aa9fb0359a52ca7c5fed5503532bf07d39
David Pham <davidpham87@gmail.com>
2015-12-02 18:14:10 +0100

    fix: Solved the kata.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,44 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as str]))
 
-(defn encode [keyword message]
-  "encodeme")
+(defn rotations [xs]
+  (let [n (count xs)]
+    (take n (partition n 1 (cycle xs)))))
 
-(defn decode [keyword message]
-  "decodeme")
+(def letters (map char (range 97 123)))
+(def letters-cypher
+  (zipmap letters (map #(zipmap letters %) (rotations letters))))
+(def letters-decypher
+  (zipmap letters (map #(zipmap % letters) (rotations letters))))
+
+(defn is-valid-candidate? [s key-word-long]
+  (.startsWith key-word-long (str/replace key-word-long s "")))
+
+(defn longest-repeated-substring [s]
+  "Assumpition is the longuest substring starts from the begining"
+  (->> (map #(apply str (take % s)) (range 1 (count s)))
+       (filter #(is-valid-candidate? % s))
+       first))
+
+(defn encode [key-word message]
+  "encodeme"
+  (->> (for [[k c] (map vector (cycle key-word) message)]
+         (get-in letters-cypher [k c]))
+       (apply str)))
+
+(defn decode [key-word message]
+  "decodeme"
+  (->> (for [[k c] (map vector (cycle key-word) message)]
+         (get-in letters-decypher [k c]))
+       (apply str)))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  "decypherme"
+  (letfn [(get-cypher-char [xs]
+            (for [[k m] xs]
+              (first (filter #(= m (get-in letters-decypher [% k]))
+                             letters))))]
+    (->> (map vector cipher message) ;; vectors of letters
+         get-cypher-char
+         (apply str)
+         longest-repeated-substring)))
