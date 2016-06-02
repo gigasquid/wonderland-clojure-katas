d46f627bd646b008f8e8b1ac9d18b35c5d3ff091
Emma Gordon <emma.gordon@projectcalico.org>
2016-06-02 15:14:33 +0100

    Add solution to tiny-maze kata (pairing exercise with Brice and John)

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,60 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def start :S)
+(def end :E)
+(def wall 1)
+(def path 0)
+
+(defn get-location [co-ord maze]
+  (get-in maze co-ord))
+
+(defn maze-coords [maze]
+  (for [i (range (count maze))
+        j (range (count (first maze)))]
+    [i j]))
+
+(defn walkable? [coord maze]
+  (if (#{start end path} (get-location coord maze)) true false))
+
+(defn escaped? [path maze]
+  (= end (get-location (last path) maze)))
+
+(defn get-start [maze]
+  (some #(and (= start (get-location % maze)) %)
+        (maze-coords maze)))
+
+(defn get-neighbours [[i j] maze]
+  (let [neighbour-coords [[(dec i) j] [i (dec j)]
+                          [(inc i) j] [i (inc j)]]]
+    (reduce #(if (#{path end} (get-location %2 maze)) (conj % %2) %)
+            [] neighbour-coords)))
+
+(defn graph [maze]
+  (->>
+    (maze-coords maze)
+    (filter #(walkable? % maze))
+    (map #(vector % (get-neighbours % maze)))
+    (into {})))
+
+(defn solver [from pathways visited]
+  (let [moves (filter #(not ((set visited) %)) (pathways from))]
+    (if (empty? moves)
+      [visited]
+      (mapcat #(solver % pathways (conj visited %)) moves))))
+
+(defn paths [maze]
+  (let [start (get-start maze)
+        graph (graph maze)
+        visited [start]]
+    (solver start graph visited)))
+
+(defn best-solution [maze]
+  (->>
+    (paths maze)
+    (filter #(escaped? % maze))
+    (sort-by count)
+    (first)))
+
+(defn solve-maze [maze]
+  (let [solution (best-solution maze)]
+    (reduce #(assoc-in % %2 :x) maze solution)))
--- tiny-maze/test/tiny_maze/solver_test.clj
+++ tiny-maze/test/tiny_maze/solver_test.clj
@@ -4,21 +4,34 @@
 
 (deftest test-solve-maze
   (testing "can find way to exit with 3x3 maze"
-    (let [maze [[:S 0 1]
-                [1  0 1]
+    (let [maze [[:S 0  1]
+                [1  0  1]
                 [1  0 :E]]
-          sol [[:x :x 1]
-               [1  :x 1]
+          sol [[:x :x  1]
+               [1  :x  1]
                [1  :x :x]]]
       (is (= sol (solve-maze maze)))))
 
     (testing "can find way to exit with 4x4 maze"
-    (let [maze [[:S 0 0 1]
-                [1  1 0 0]
-                [1  0  0 1]
+    (let [maze [[:S 0  0  1]
+                [1  1  0  0]
+                [1  0  0  1]
                 [1  1  0 :E]]
-          sol [[:x :x :x 1]
-                [1  1 :x 0]
-                [1  0 :x 1]
-                [1  1  :x :x]]]
-     (is (= sol (solve-maze maze))))))
+          sol [[:x :x :x  1]
+               [1   1 :x  0]
+               [1   0 :x  1]
+               [1   1 :x :x]]]
+     (is (= sol (solve-maze maze)))))
+
+    (testing "can find way to exit when maze contains a loop"
+        (let [maze [[:S 0  0  0  1  1]
+                    [0  1  1  0  1  1]
+                    [0  1  0  0  0  1]
+                    [0  1  0  1  0  1]
+                    [0  0  0  1  0 :E]]
+              sol [[:x :x :x :x  1  1]
+                   [0   1  1 :x  1  1]
+                   [0   1  0 :x :x  1]
+                   [0   1  0  1 :x  1]
+                   [0   0  0  1 :x :x]]]
+        (is (= sol (solve-maze maze))))))
