2b040fd67b341b80b0d786cda544c5be05b790ec
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-06-13 00:28:37 +0200

    wip: decipher doesn't work yet

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,23 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def ALPHABET "abcdefghijklmnopqrstuvwxyz")
 
-(defn decode [keyword message]
-  "decodeme")
+(def indexed-alphabet
+  (zipmap ALPHABET (iterate inc 0)))
+
+(defn chars-for-col [col]
+  (take (count ALPHABET) (drop (get indexed-alphabet col) (cycle ALPHABET))))
+
+(defn col->char [col row] (get (zipmap (chars-for-col col) ALPHABET) row))
+(defn col->row [col char] (get (zipmap ALPHABET (chars-for-col col)) char))
+(defn map-matrix [mapper keyword message]
+  (let [pairs (map vector (cycle keyword) message)
+        encoded (map #(apply mapper %) pairs)]
+    (apply str encoded)))
+
+(defn encode [keyword message] (map-matrix col->row keyword message))
+(defn decode [keyword message] (map-matrix col->char keyword message))
+
 
 (defn decipher [cipher message]
   "decypherme")
-
