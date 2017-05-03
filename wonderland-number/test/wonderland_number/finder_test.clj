b47a86a4903d82db838095969a2700db68a662e5
pwentz <patrick.w.wentz@gmail.com>
2017-05-03 13:42:43 -0500

    first iteration complete

--- wonderland-number/test/wonderland_number/finder_test.clj
+++ wonderland-number/test/wonderland_number/finder_test.clj
@@ -7,6 +7,36 @@
         s2 (set (str n2))]
     (= s1 s2)))
 
+(deftest test-six-digits
+  (testing "it can tell whether a number has 6 digits"
+    (is (= true (six-digits? 123456)))
+    (is (= false (six-digits? 12345)))))
+
+(deftest test-same-digits-after-double
+  (testing "it can tell whether number has same digits after it doubles"
+    (is (= true (same-digits-as-product? 2 142857)))
+    (is (= false (same-digits-as-product? 2 123)))))
+
+(deftest test-same-digits-after-triple
+  (testing "it can tell whether number has same digits after it triples"
+    (is (= true (same-digits-as-product? 3 142857)))
+    (is (= false (same-digits-as-product? 3 123)))))
+
+(deftest test-same-digits-after-quad
+  (testing "it can tell whether number has same digits after it quadruples"
+    (is (= true (same-digits-as-product? 4 142857)))
+    (is (= false (same-digits-as-product? 4 123)))))
+
+(deftest test-same-digits-after-quin
+  (testing "it can tell whether number has same digits after it quintuples"
+    (is (= true (same-digits-as-product? 5 142857)))
+    (is (= false (same-digits-as-product? 5 123)))))
+
+(deftest test-same-digits-after-six
+  (testing "it can tell whether number has same digits after it sextuples"
+    (is (= true (same-digits-as-product? 6 142857)))
+    (is (= false (same-digits-as-product? 6 123)))))
+
 (deftest test-wonderland-number
   (testing "A wonderland number must have the following things true about it"
     (let [wondernum (wonderland-number)]
