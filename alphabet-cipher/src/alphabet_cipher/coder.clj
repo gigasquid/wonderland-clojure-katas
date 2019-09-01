e4bfc5a153ca44695f1811e743104f34dd225b01
TK <leandrotk100@gmail.com>
2019-08-31 21:46:43 -0300

    Reset

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,57 +1,7 @@
-(ns alphabet-cipher.coder
-  (:require [clojure.string :as string]))
-
-(defn make-string-with-same-length-as-other [str1 str2]
-  (loop [str1 str1
-         str2 str2]
-    (if (>= (count str1) (count str2))
-      (subs str1 0 (count str2))
-      (recur (str str1 str1) str2))))
-
-(defn letter->index [str]
-  (string/index-of "abcdefghijklmnopqrstuvwxyz" str))
-
-(defn get-matrix-element [row-column-pair]
-  (let [[row column] row-column-pair
-        matrix ["abcdefghijklmnopqrstuvwxyz"
-                "bcdefghijklmnopqrstuvwxyza"
-                "cdefghijklmnopqrstuvwxyzab"
-                "defghijklmnopqrstuvwxyzabc"
-                "efghijklmnopqrstuvwxyzabcd"
-                "fghijklmnopqrstuvwxyzabcde"
-                "ghijklmnopqrstuvwxyzabcdef"
-                "hijklmnopqrstuvwxyzabcdefg"
-                "ijklmnopqrstuvwxyzabcdefgh"
-                "jklmnopqrstuvwxyzabcdefghi"
-                "klmnopqrstuvwxyzabcdefghij"
-                "lmnopqrstuvwxyzabcdefghijk"
-                "mnopqrstuvwxyzabcdefghijkl"
-                "nopqrstuvwxyzabcdefghijklm"
-                "opqrstuvwxyzabcdefghijklmn"
-                "pqrstuvwxyzabcdefghijklmno"
-                "qrstuvwxyzabcdefghijklmnop"
-                "rstuvwxyzabcdefghijklmnopq"
-                "stuvwxyzabcdefghijklmnopqr"
-                "tuvwxyzabcdefghijklmnopqrs"
-                "uvwxyzabcdefghijklmnopqrst"
-                "vwxyzabcdefghijklmnopqrstu"
-                "wxyzabcdefghijklmnopqrstuv"
-                "xyzabcdefghijklmnopqrstuvw"
-                "yzabcdefghijklmnopqrstuvwx"
-                "zabcdefghijklmnopqrstuvwxy"]]
-    (get (get matrix row) column)))
-
-(defn make-row-column-pair [pair]
-  [(letter->index (first pair)) (letter->index (second pair))])
+(ns alphabet-cipher.coder)
 
 (defn encode [keyword message]
-  (let [keyword (make-string-with-same-length-as-other keyword message)
-        message message]
-    (->> keyword
-         (map vector message)
-         (map make-row-column-pair)
-         (map get-matrix-element)
-         (reduce str))))
+  "encodeme")
 
 (defn decode [keyword message]
   "decodeme")
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,35 +2,6 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
-(deftest test-make-string-with-same-length-as-other
-  (testing "Make one string with the same length as other one"
-    (is (= "vigilancevigilancevigilancevi"
-           (make-string-with-same-length-as-other "vigilance" "meetmeontuesdayeveningatseven")))
-    (is (= "onestringon"
-           (make-string-with-same-length-as-other "onestring" "otherstring")))
-    (is (= "same-length"
-           (make-string-with-same-length-as-other "same-length" "same-length")))
-    (is (= "sconessconessco"
-           (make-string-with-same-length-as-other "scones" "meetmebythetree")))))
-
-(deftest test-letter->index
-  (testing "Map a string letter into a index integer"
-    (is (= (letter->index "a") 0))
-    (is (= (letter->index "b") 1))
-    (is (= (letter->index "z") 25))))
-
-(deftest test-get-matrix-element
-  (testing "Get element from matrix passing the row and column"
-    (is (= (get-matrix-element [0 0]) \a))
-    (is (= (get-matrix-element [0 1]) \b))
-    (is (= (get-matrix-element [1 2]) \d))
-    (is (= (get-matrix-element [30 22]) nil))))
-
-(deftest test-make-row-column-pair
-  (testing "Make row-column pair based on the matrix of strings"
-    (is (= (make-row-column-pair [\s \m]) [18 12]))
-    (is (= (make-row-column-pair [\c \e]) [2 4]))))
-
 (deftest test-encode
   (testing "can encode a message with a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
