d0f05ab6a6e0315e378d606b205b6c64df0d9b1f
Luciana Marques <luciana.marques@luciana.marques>
2019-12-27 20:53:25 -0300

    Remove suit tests from game-war

--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -9,10 +9,7 @@
     (is (= 0 1)))
   (testing "queens are higher rank than jacks")
   (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+  (testing "aces are higher rank than kings"))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
